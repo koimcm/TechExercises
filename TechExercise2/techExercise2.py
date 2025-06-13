b8db112246ea667a67b2a2fbd26d0a983df53fbe
Koi McManis <125946764+koimcm@users.noreply.github.com>
2025-06-12 20:44:02 -0500

    Update techExercise2.py

--- TechExercise2/techExercise2.py
+++ TechExercise2/techExercise2.py
@@ -1,1 +1,16 @@
+from flask import Flask
+# Create web app
+app = Flask(__name__)
 
+# Page where app says hello to user
+@app.route('/hello/<name>')
+def hello(name):
+    return "Hello %s!" % name
+
+# Default hello world page
+@app.route("/")
+def helloWorld():
+    return "Hello World! go to /hello/yourName for more! (yourName is any variable)"
+# Main function
+if __name__ == "__main__":
+    app.run()
